1dea9d710018e3ba32240f96e70e2d759343fb3d
Vince Navarro <vincent@vincentnavarro.com>
2021-05-08 22:21:09 -0700

    Working image

--- src/components/series/series.js
+++ src/components/series/series.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { getRandomSeries } from "../../services/tv-maze-api-client";
+import "./series.css";
 
 export default function Series({ currentSeries }) {
-  const { name, premiered, summary } = currentSeries || {};
+  const { name, premiered, summary, image } = currentSeries || {};
 
   const formattedGenres =
     currentSeries && currentSeries.genres.length
@@ -12,13 +13,20 @@ export default function Series({ currentSeries }) {
   const formattedPremieredDate = premiered ? `Premiered on ${premiered}` : "";
 
   return (
-    <div>
-      <h1 className="seriesName">{name}</h1>
-      <div className="seriesInfo">
-        {formattedGenres}
-        {formattedPremieredDate}
+    <div className="container">
+      <div className="row">
+        <div className="image">
+          {image ? <img src={image} alt="Series Preview" /> : "N/A"}
+        </div>
+        <div className="col-sm-8">
+          <h1 className="seriesName">{name}</h1>
+          <div className="seriesInfo">
+            {formattedGenres}
+            {formattedPremieredDate}
+          </div>
+          <div className="seriesSummary">{summary}</div>
+        </div>
       </div>
-      <div className="seriesSummary">{summary}</div>
     </div>
   );
 }
--- src/services/utils/tv-maze-util.js
+++ src/services/utils/tv-maze-util.js
@@ -16,5 +16,6 @@ export const formatSeries = (series) => {
     genres: series.genres && series.genres.length ? series.genres : [],
     premiered: series.premiered ? formatDate(series.premiered) : "",
     summary: series.summary ? formatSummary(series.summary) : "",
+    image: series.image && series.image.medium ? series.image.medium : "",
   };
 };
